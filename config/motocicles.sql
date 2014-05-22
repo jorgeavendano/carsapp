634ce6051e48b91ced5f8c1fe72b4d6b4cdfad29
nboccolini <nico_bocco@live.com>
2014-05-22 20:45:12 -0300

    Base de datos carsApp version 1.1

--- config/motocicles.sql
+++ config/motocicles.sql
@@ -2,5 +2,6 @@ DROP TABLE IF EXISTS motocicles;
 CREATE TABLE motocicles (
 			id_motocicles INT(11) NOT NULL AUTO_INCREMENT,
 			patent INT,
-			CONSTRAINT pk_tmotocicles PRIMARY KEY (id_motocicles),
+			cc INT,
+			CONSTRAINT pk_motocicles PRIMARY KEY (id_motocicles),
 		    CONSTRAINT fk_motocicles_vehicle FOREIGN KEY (patent) REFERENCES vehicles (patents) ON DELETE CASCADE ON UPDATE CASCADE);
--- config/trucks.sql
+++ config/trucks.sql
@@ -2,5 +2,6 @@ DROP TABLE IF EXISTS trucks;
 CREATE TABLE trucks (
 		  id_trucks INT(11) NOT NULL AUTO_INCREMENT,
 		  patent INT,
+		  capacity VARCHAR(20),
 		  CONSTRAINT pk_trucks PRIMARY KEY (id_trucks),
 		  CONSTRAINT fk_trucks_vehicle FOREIGN KEY (patent) REFERENCES vehicles (patents) ON DELETE CASCADE ON UPDATE CASCADE);
