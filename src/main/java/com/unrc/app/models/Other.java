a2e1ad92f815744fcb9d93a4caee7967bb1a06e9
nicoboccolini <nico_bocco@live.com>
2014-05-22 21:48:29 -0300

    Update Other.java

--- src/main/java/com/unrc/app/models/Other.java
+++ src/main/java/com/unrc/app/models/Other.java
@@ -1,10 +1,6 @@
 package com.unrc.app.models;
 import org.javalite.activejdbc.Model;
 
-/**
- *
- * @author jorgea
- */
 public class Other extends Model{
       static {
     	  validatePresenceOf("patents","transmission");
