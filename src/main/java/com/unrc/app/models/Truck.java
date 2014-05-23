cc5e70dc890393a7296a59050d014fe8ec1d0e2a
nicoboccolini <nico_bocco@live.com>
2014-05-22 21:47:05 -0300

    Update Truck.java

--- src/main/java/com/unrc/app/models/Truck.java
+++ src/main/java/com/unrc/app/models/Truck.java
@@ -1,10 +1,6 @@
 package com.unrc.app.models;
 import org.javalite.activejdbc.Model;
 
-/**
- *
- * @author jorgea
- */
 public class Truck extends Model{
       static {
     	  validatePresenceOf("patents","capacity");
