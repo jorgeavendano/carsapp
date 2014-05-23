3b590e74d5b3030902e79dbc2d3d48ce86e51c5f
Lapianas <santi_lapiana@hotmail.com>
2014-05-23 06:48:30 -0300

    Update Car.java

--- src/main/java/com/unrc/app/models/Car.java
+++ src/main/java/com/unrc/app/models/Car.java
@@ -1,10 +1,7 @@
 package com.unrc.app.models;
 import org.javalite.activejdbc.Model;
 
-/**
- *
- * @author jorgea
- */
+
 public class Car extends Model{
       static {
       validatePresenceOf("patents","isCoupe");
