498e9638b4d268c417bd67b6e93c5ee2fe211a1f
nicoboccolini <nico_bocco@live.com>
2014-05-22 21:48:01 -0300

    Update Vehicle.java

--- src/main/java/com/unrc/app/models/Vehicle.java
+++ src/main/java/com/unrc/app/models/Vehicle.java
@@ -2,10 +2,6 @@
 package com.unrc.app.models;
 import org.javalite.activejdbc.Model;
 
-/**
- *
- * @author jorgea
- */
 public class Vehicle extends Model{
       static {
       validatePresenceOf("mark", "model","patents","id_user");
