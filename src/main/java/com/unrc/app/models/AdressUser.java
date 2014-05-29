05a99cdbdb074f7aff219d0f1a1e4ab033a3ea3f
Lapianas <santi_lapiana@hotmail.com>
2014-05-29 18:10:23 -0300

    Java dl modelo que relaciona users y direcciones
    java del adress user, almacena las relaciones entre usuarios y
    direcciones de los mismos

---
+++ src/main/java/com/unrc/app/models/AdressUser.java
@@ -0,0 +1,8 @@
+package com.unrc.app.models;
+
+import org.javalite.activejdbc.Model;
+
+//Modelo que almacena las relaciones entre Usuarios y direcciones
+public class UsersAddress extends Model {
+
+}
