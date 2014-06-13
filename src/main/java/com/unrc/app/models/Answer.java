bf9e9e6ba146ca9c7b5eb3db9e770c8a71e3e2b2
nboccolini <nico_bocco@live.com>
2014-06-13 16:57:53 -0300

    Update

--- src/main/java/com/unrc/app/models/Answer.java
+++ src/main/java/com/unrc/app/models/Answer.java
@@ -1,17 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 package com.unrc.app.models;
 
 import org.javalite.activejdbc.Model;
 
-/**
- *
- * @author jorgea
- */
 public class Answer extends Model{
     static {
         validatePresenceOf("id_user","description","id_question");
--- src/main/java/com/unrc/app/models/UsersAddress.java
+++ src/main/java/com/unrc/app/models/UsersAddress.java
@@ -2,8 +2,31 @@ package com.unrc.app.models;
 import org.javalite.activejdbc.Model;
 import org.javalite.activejdbc.annotations.Table;
 
-
-
 public class UsersAddress extends Model {
+    static {
+        validatePresenceOf("id_user","id_address");
+    }   
+    
+    public static UsersAddress findByUsersAddress(int id_user, int id_address){
+	return (findFirst("id_user = ? and id_address= ?",  id_user,id_address));
+    }
+
+    public static UsersAddress UsersAddress(int id_user, int id_address){
+        UsersAddress answer =create("id_user",id_user, "id_address",id_address);
+        answer.saveIt();
+        return findByUsersAddress(id_user,id_address);
+    }
+
+    public static Boolean existUsersAddress(int id_user,int id_address){
+        return (UsersAddress.first("id_user = ? and id_address = ? ", id_user,id_address) != null);
+    }
+   
+    public static void deleteUsersAddress(int id_user,int id_address){
+        if(existUsersAddress(id_user,id_address)){
+            UsersAddress.delete("id_user = ? and id_question = ? ",id_user, id_address);
+        }
+    }   
+    
+    
 
 }
