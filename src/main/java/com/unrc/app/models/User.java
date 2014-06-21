4744abeeab809b9e48939bdd879e9208fdb52657
jorgeavendano <jorge.avendanor@autonoma.edu.co>
2014-06-21 20:02:24 -0300

    elasticsearch

--- src/main/java/com/unrc/app/models/User.java
+++ src/main/java/com/unrc/app/models/User.java
@@ -1,11 +1,39 @@
 package com.unrc.app.models;
 
+import com.unrc.app.ElasticSearch;
+import java.util.HashMap;
+import java.util.Map;
+import org.elasticsearch.action.get.GetResponse;
 import org.javalite.activejdbc.Model;
 
 public class User extends Model {
     static {
       validatePresenceOf("first_name", "last_name", "email", "contrasena");
     }
+     @Override
+    public void afterCreate(){
+
+      Map<String, Object> json = new HashMap<>();
+      json.put("name", this.get("first_name"));
+      json.put("email", this.get("email"));
+      json.put("id", this.getId());
+               System.out.println(this.getId()+"  ----este es el id---");
+      ElasticSearch.client().prepareIndex("users", "user")
+                  .setSource(json)
+                  .execute()
+                  .actionGet();
+      prueba();
+    }
+        public void prueba(){
+                
+            
+            GetResponse response = ElasticSearch.client().prepareGet("users", "user", "nG9juRAnSfquhuQTxbAVLQ")
+        .execute()
+        .actionGet();
+            System.out.println("---------------------------consulta--------------------------");
+            System.out.println(response.getIndex());
+        }
+    
     
     public static User findByEmail(String email){
 	return (findFirst("email = ?", email));
